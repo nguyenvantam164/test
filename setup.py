079e67638f184c647f1db2c622444729f0345cce
nguyenvantam164 <nguyenvantamdk2@gmail.com>
2018-12-10 14:47:42 +0700

    add setup file for version 0.2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="test",
+    version="0.2",
+    author_email="phelan@cinnamon.is",
+    description="this is test repo",
+    py_modules = ['test.py']
+)
